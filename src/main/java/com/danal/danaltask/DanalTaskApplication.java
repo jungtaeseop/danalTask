ce6f44eb5e9f546d17fe37cabb546bda05920e39
rrew77@nexpa.co.kr <rrew77@nexpa.co.kr>
2024-06-10 09:54:10 +0900

    first commit

---
+++ src/main/java/com/danal/danaltask/DanalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.danal.danaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanalTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danal/danaltask/DanalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danal.danaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
